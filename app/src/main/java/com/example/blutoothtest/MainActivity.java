c6ccc059ab9d245f009b53aba212bab8f08fd3f5
Amro Sayed Ahmed <s0572938@htw-berlin.de>
2020-11-25 00:17:16 +0300

    MainActivityStyle

--- app/src/main/java/com/example/blutoothtest/MainActivity.java
+++ app/src/main/java/com/example/blutoothtest/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.blutoothtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        String[] userArray = {"User 1", "User 2", "User 3", "User 4",
+                "User 5", "User 6", "User 7", "User 8", "User 9", "User 10", "User 11"};
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,
+                R.layout.activity_main, userArray);
+
+        ListView listView = (ListView) findViewById(R.id.liste);
+        listView.setAdapter(adapter);
     }
 }
